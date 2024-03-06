d4c585c552a3528765440f5aa411988cf251fa24
ZodiaComs <125735027+ZodiaComs@users.noreply.github.com>
2024-03-06 17:55:02 +0800

    Add files via upload

---
+++ PHP/Week01/name.php
@@ -0,0 +1,24 @@
+<html>
+
+    <head>
+        <title> php homework </title>
+    </head>
+
+    <body>
+        <?php
+            echo "hello world";
+            echo "<br>";
+            echo "<b> hello php </b>";
+            echo "<br>";
+
+            $a = 10;
+            $b = 20;
+
+            if($a < $b)
+            {
+                echo "that's that and this is this.";
+            }
+        ?>
+    </body>
+
+</html>
