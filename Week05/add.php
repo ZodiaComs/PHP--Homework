1cd224d2aa6f7f421d33944977e7e7d19e8d54fb
ZodiaComs <125735027+ZodiaComs@users.noreply.github.com>
2024-06-17 21:37:49 +0800

    Delete Week05 directory

--- Week05/add.php
+++
@@ -1,9 +0,0 @@
-<meta charset='utf8'>
-
-<form action="adddb.php" method="post">
-student ID: <input type="text" name="sID"><br/>
-Name: <input type="text" name="sName"><br/>
-Department: <input type="text" name="sDept"><br/>
-E-mail: <input type="email" name="sEmail"><br/>
-<input type="submit" value="submit">
-</form>
