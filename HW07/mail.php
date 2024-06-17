187426bc6ddfaa34c1850d99aca651c71a997954
ZodiaComs <125735027+ZodiaComs@users.noreply.github.com>
2024-06-17 21:40:15 +0800

    Add files via upload

---
+++ HW07/mail.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<meta charset="utf-8">
+<title>Send Advertisement Email</title>
+</head>
+<body>
+<h2>Send advertising E-mail</h2>
+<form method="post" action="sendmail.php">
+  To: <input type = "email" name = "to"><br/>
+  title: </title>: <input type="text" name="subject"><br>
+  content: <textarea name="content"></textarea><br>
+  <input type="submit" value="Send">
+</form>
+</body>
+</html>
