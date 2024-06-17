2a83a285bf1f5c3a3ce3463bc7f083cc8ce4d7c5
ZodiaComs <125735027+ZodiaComs@users.noreply.github.com>
2024-06-17 21:36:27 +0800

    Add files via upload

---
+++ HW02/cv.php
@@ -0,0 +1,41 @@
+﻿<html>
+
+<head>
+<meta charset = "utf-8">
+<meta name = "title name">
+<title> curriculum vitae </title>
+</head>
+
+<body>
+<center> <h1> 章偉健 - CV </h1> </center>
+<hr width ="50%" color = "black">
+<br />
+<center> <img src = "pasfoto.jpg" width = "250" height = "50%" border = "1" alt = "pasfoto"> </center>
+<br />
+<h2> Personal Info </h2>
+name = Bertrand anthony
+<br />
+date of birth = 2004/09/30
+<br />
+gender = male
+<br />
+nationality = indonesian
+<br />
+highest education = highschool
+<br />
+current major = Information Management
+<br />
+skills = teamwork, able to communicate ideas
+<br />
+hobby = play table tennis, listening to music, play videogames
+<br />
+<br />
+<hr width ="25%" color = "black" align = "left">
+<h1> Contact </h1>
+<table border ="1" bgcolor = "#fa6607">
+<tr bgcolor = "#089bcc"> <th> phone number </th> <th> 0967034598 </th> </tr>
+<tr> <th> email address </th> <th> bertrand.anthony@gmail.com </th> </tr>
+<tr> <th> address </th> <th> No. 700號, Gaoxiongdaxue Rd, Nanzih District, Kaohsiung City, 811 </th> </tr>
+
+</body>
+</html>
